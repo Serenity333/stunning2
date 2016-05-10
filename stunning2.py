c0d3815fcddcd9a371321ef9dcfedfe18263f984
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-05-10 16:58:48 -0400

    Create stunning2.py

---
+++ stunning2.py
@@ -0,0 +1,12 @@
+#code to create a fun pattern with a Turtle via Python
+
+from turtle import *
+color('blue', 'green')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
